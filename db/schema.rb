6e33477e425a2b780e1c1f90eea6ca8f63432119
zijingkek <chrisartbp@gmail.com>
2020-07-01 01:36:50 +0800

    remove morning and afternoon string from venues table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_111850) do
+ActiveRecord::Schema.define(version: 2020_06_30_173559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,8 +75,6 @@ ActiveRecord::Schema.define(version: 2020_06_30_111850) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "morning"
-    t.string "afternoon"
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
 
