d9d44cd366b7d1994f2f32d3fe52baa02c24b932
zijingkek <chrisartbp@gmail.com>
2020-07-01 01:29:38 +0800

    update booking routes- edit,update,destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
@@ -6,6 +5,8 @@ Rails.application.routes.draw do
   get '/account/profile', to: "pages#profile"
   get '/account/bookings', to: "pages#bookings"
   get '/account/bookings/:id/edit', to: "pages#edit"
+  patch '/account/bookings/:id', to: "pages#update"
+  delete '/account/bookings/:id', to: "pages#destroy"
   get '/account/my_listings', to: "pages#my_listings"
   get '/s', to: "pages#search_result"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
