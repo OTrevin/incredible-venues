ffbc668d94f1030a481185fe4507d6749c875c1c
zijingkek <chrisartbp@gmail.com>
2020-07-01 16:02:23 +0800

    booking controller all done

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,15 @@ Rails.application.routes.draw do
   get '/account', to: "pages#account"
   get '/account/profile', to: "pages#profile"
   get '/account/bookings', to: "pages#bookings"
-  get '/account/bookings/:id/edit', to: "pages#edit"
-  patch '/account/bookings/:id', to: "pages#update"
-  delete '/account/bookings/:id', to: "pages#destroy"
+  get '/account/bookings/:id/edit', to: "pages#edit", as: :edit_booking
+  # patch '/account/bookings/:id', to: "pages#update"
+  delete '/account/bookings/:id', to: "pages#destroy", as: :delete_booking
   get '/account/my_listings', to: "pages#my_listings"
   get '/s', to: "pages#search_result"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :venues
+  resources :venues do
+    resources :bookings, only: [:new, :create, :update]
+    # resources :bookings, only: [:new, :create, :edit, :update]
+  end
+   # resources :bookings, only: [:update]
 end
